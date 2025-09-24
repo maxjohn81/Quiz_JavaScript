48f7d3f53047eaf6959331f642cb7ae5990e4a84
maxjohn81 <rivoandriharisoa@gmail.com>
2025-09-24 11:53:05 +0300

    Quiz

---
+++ src/main.js
@@ -0,0 +1,146 @@
+import "./style.css";
+import { quizThemes } from "./question";
+
+let app = document.getElementById("app");
+let titre = document.getElementById("tittre");
+let commencer = document.getElementById("commencer");
+
+commencer.addEventListener("click", () => {
+  startQuiz();
+
+});
+
+function startQuiz(event) {
+  let countQuestion = 0;
+  let score = 0;
+
+  // let temps = 2;
+  // let second = document.getElementById("second");
+
+  // function moins() {
+  //   if (temps == 0) {
+  //     afficher();
+  //     submit()
+  //   }
+  //   setInterval(() => {
+  //     temps -= 1;
+  //     second.innerHTML = temps;
+  //   }, 1000);
+  // }
+  // moins()
+
+
+  afficher();
+
+  function afficher() {
+    while (app.firstElementChild) {
+      app.firstElementChild.remove();
+    }
+  }
+
+  commencerQuestion(countQuestion);
+
+  function commencerQuestion(index) {
+    afficher();
+    const question = quizThemes[index];
+
+    if (!question) {
+      finDeLaPartie();
+      return;
+    }
+
+    let title = getTitleElement(question.question);
+    app.appendChild(title);
+    const answerDiv = creatAnswers(question.choices);
+    app.appendChild(answerDiv);
+
+    const submitBouton = getSubmitBouton();
+    app.appendChild(submitBouton);
+
+    submitBouton.addEventListener("click", () => {
+      submit();
+    });
+  }
+
+  function submit() {
+    const selectedAnswer = app.querySelector('input[name="choice"]:checked');
+    const value = selectedAnswer.value;
+
+    const question = quizThemes[countQuestion];
+    const isCorrect = question.answer === value;
+    if (isCorrect) {
+      score++;
+    }
+
+    plus();
+  }
+  function plus() {
+    setTimeout(() => {
+      countQuestion++;
+      commencerQuestion(countQuestion);
+    }, 10);
+  }
+  function finDeLaPartie() {
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+    h1.innerHTML = "Quiz terminé !";
+    let fel = "Malheureusement";
+    if (score > 3 && score < 6) {
+      fel = "Bravo";
+      p.innerHTML = `${fel} tu as eu ${score} sur ${quizThemes.length}`;
+      p.classList.add("moyen");
+    } else if(score > 6) {
+      fel = "Chapeau"
+      p.innerHTML = `${fel} tu as eu ${score} sur ${quizThemes.length}`;
+      p.classList.add("moyen");
+    }else{
+      p.innerHTML = `${fel} tu as eu ${score} sur ${quizThemes.length}`;
+      p.classList.add("bas");
+    }
+
+    app.appendChild(h1);
+    app.appendChild(p);
+
+    setTimeout(() => {
+      alert("Voulez vous recommencer la partie")
+      startQuiz()
+    }, 5000);
+  }
+
+  function creatAnswers(choices) {
+    const answerDiv = document.createElement("div");
+    answerDiv.classList.add("answers");
+
+    for (const choice of choices) {
+      const label = getAnswerElement(choice);
+      label.classList.add("label");
+      answerDiv.appendChild(label);
+    }
+    return answerDiv;
+  }
+}
+
+function getSubmitBouton() {
+  const button = document.createElement("button");
+  button.innerText = "Valider";
+  button.classList.add("submit");
+  return button;
+}
+
+function getTitleElement(texte) {
+  let title = document.createElement("h3");
+  title.innerText = texte;
+  return title;
+}
+
+function getAnswerElement(texte) {
+  const label = document.createElement("label");
+  label.innerText = texte;
+  const input = document.createElement("input");
+  input.id = texte.replaceAll(" ", "_").toLowerCase();
+  input.setAttribute("type", "radio");
+  input.setAttribute("name", "choice");
+  input.setAttribute("value", texte);
+  label.appendChild(input);
+  return label;
+}
---
+++ src/question.js
@@ -0,0 +1,38 @@
+// Objet principal des questions
+export const quizThemes = [
+  {
+    question: "Quelle est la capitale de la France ?",
+    choices: ["Paris", "Lyon", "Marseille", "Bordeaux"],
+    answer: "Paris", // index de la réponse correcte dans choices
+  },
+  {
+    question: "2 + 2 = ?",
+    choices: ["3", "4", "5", "22"],
+    answer: "4",
+  },
+  {
+    question: "Quel langage est utilisé pour le style d'une page web ?",
+    choices: ["HTML", "Python", "CSS", "C++"],
+    answer: "CSS",
+  },
+  {
+    question: "Quel jeu a pour personnage principal 'Mario' ?",
+    choices: ["Zelda", "Mario Kart", "Super Mario Bros", "Metroid"],
+    answer: "Super Mario Bros",
+  },
+  {
+    question: "Quelle console est sortie en 2020 par Sony ?",
+    choices: ["PS3", "PS4", "PS5", "PS2"],
+    answer: "PS5",
+  },
+  {
+    question: "L'eau gèle à quelle température (°C) ?",
+    choices: ["0", "100", "-10", "32"],
+    answer: "0",
+  },
+  {
+    question: "Quelle est la planète la plus proche du Soleil ?",
+    choices: ["Terre", "Mars", "Mercure", "Vénus"],
+    answer: "Mercure",
+  },
+];
